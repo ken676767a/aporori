a5341a3fcd04d788b304fbc178709ee365e356f4
ken676767a <86898870+ken676767a@users.noreply.github.com>
2021-07-06 13:37:28 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,35 +4,39 @@
 #include<math.h>
 #include<stdlib.h>
 #include<time.h>
+#include<map>
 using namespace std;
 void ffind(int,int,int,vector <vector<int>>);
 short atoii(string);
-void ap(vector <vector<short>>,vector<short>,int,vector<short>);
+void ap(vector < map<short,int>>*,map<short,int>*,vector<short>);
 vector<vector<int>>re;
 vector<string>finn;
 int ma=0;
 int answer=0;
+float start=clock();
+float aptime=0;
 int main()
 
 {
-float start=clock();
-vector <string>orig ;
-vector <short>tem ;
-vector <vector<short>> peo;
-vector<short>can;
-int te,n;
-n=0;
-te=0;
-string line;
-fstream fin;
-string testr;
-    fin.open("T15I7N0.5KD10K.txt",ios::in);
+    ios_base::sync_with_stdio(false);
+
+    vector <string>orig ;
+    map<short,int>tem ;
+    vector < map<short,int>> peo;
+    map<short,int>can;
+    int te;
+
+    te=0;
+    string line;
+    fstream fin;
+    string testr;
+    fin.open("T15I7N0.5KD10K.txt",ios::in);//T15I7N0.5KD1K
     while(getline(fin,line))
     {
         line+=",";
         orig.push_back(line);
 
-       // cout<<"line = "<<line<<endl;
+        // cout<<"line = "<<line<<endl;
 
     }
     //cout<<"orig.size() = "<<orig.size()<<endl;
@@ -42,10 +46,10 @@ string testr;
 
             //cout<<orig.at(i)<<endl;
         }*/
-        cout<<'\n';
-    for(int i=0;i<orig.size();i++)
+    cout<<'\n';
+    for(int i=0; i<orig.size(); i++)
     {
-        for(int j=0;j<orig.at(i).size();j++)
+        for(int j=0; j<orig.at(i).size(); j++)
         {
 
 
@@ -65,7 +69,7 @@ string testr;
 
                 if(testr.length()!=0)
                 {
-                    tem.push_back(atoii(testr));
+                    tem.insert(pair<short,int>(atoii(testr),1));
                     testr="";
                 }
             }
@@ -83,138 +87,180 @@ string testr;
 
 
     }
-
-    for(int i=0;i<500;i++)
+    /*for(int j=0; j<peo.size(); j++)
     {
-        for(int j=0;j<peo.size();j++)
+        for(auto k=peo.at(j).begin();k!=peo.at(j).end();k++)
         {
-            for(int k=0;k<peo.at(j).size();k++)
-            {
-                if (peo.at(j).at(k)==i)
-                {
-                    n++;
-                }
-            }
-        }
-        if(n<50)
-        {
-            for(int j=0;j<peo.size();j++)
-            {
-                for(int k=0;k<peo.at(j).size();k++)
-                {
-                    if (peo.at(j).at(k)==i)
-                    {
-                        peo.at(j).erase(peo.at(j).begin()+k);
-                    }
-
-                }
-            }
-
-        }
-        else
-        {
-            can.push_back(i);
-            //answer++;
-        }
-        n=0;
-    }
-    cout<<answer<<endl;
-    vector<short>now;
-    now.clear();
-
-
-    ap(peo,can,0,now);
-    /*for(int i=0;i<re.size();i++)
-    {
-        for(int j=0;j<re.at(i).size();j++)
-        {
-            cout<<re.at(i).at(j)<<" ";
+            cout<<k->first<<" ";
         }
         cout<<endl;
     }*/
-    float endd=clock();
-    cout<<answer<<endl;
-    cout<<(endd-start)/1000;
 
 
 
 
-
-return 0;}
-
-
-short atoii(string str)
-{
-
-    int te=0;
-    int j=str.length();
-                     for(int k=1;str.length()>=k;k++)
-                {
-                    //cout<<(int)(orig.at(i)[j-k]);
-                   //cout<<(int)(orig.at(i)[j-k])-48<<endl;
-
-
-                    te=((short)(str[j-k])-48)*pow(10,k-1)+te;
-                   // cout<<te<<endl;
-
-                }
-
-                return te;
-}
-void ap(vector <vector<short>>peo,vector<short>cand,int ind,vector<short>now)
-{
-    int nind=ind;
-
-
-    for(int i=ind;i<cand.size();i++)
-    {
-        int n=0;
-
-        vector <vector<short>>npeo;
-        for(int j=0;j<peo.size();j++)
+        for(short i=0; i<500; i++)
         {
-
-            for(int k=0;k<peo.at(j).size();k++)
+            int n=0;
+            for(int j=0; j<peo.size(); j++)
             {
-                if(cand.at(i)==peo.at(j).at(k))
+                map<short,int>::iterator k=peo.at(j).find(i);
+                if(k!=peo.at(j).end())
                 {
                     n++;
-                    npeo.push_back(peo.at(j));
+                }
+
+            }
+
+            if(n<50)
+            {
+                for(int j=0; j<peo.size(); j++)
+                {
+
+                    peo.at(j).erase(i) ;
+                }
+
+            }
+            else
+            {
+                can.insert(pair<short,int>(i,1));
+                //answer++;
+            }
+            n=0;
+        }
+
+
+
+
+        vector<short>now;
+        now.clear();
+
+
+        ap(&peo,&can,now);
+        /*for(int i=0;i<re.size();i++)
+        {
+            for(int j=0;j<re.at(i).size();j++)
+            {
+                cout<<re.at(i).at(j)<<" ";
+            }
+            cout<<endl;
+        }*/
+        float endd=clock();
+        cout<<answer<<'\n';
+        cout<<(endd-start)/1000;
+
+
+    printf("ap_time = %f\n",aptime);
+
+
+        return 0;
+    }
+
+
+    short atoii(string str)
+    {
+
+        int te=0;
+        int j=str.length();
+        for(int k=1; str.length()>=k; k++)
+        {
+            //cout<<(int)(orig.at(i)[j-k]);
+            //cout<<(int)(orig.at(i)[j-k])-48<<endl;
+
+
+            te=((short)(str[j-k])-48)*pow(10,k-1)+te;
+            // cout<<te<<endl;
+
+        }
+
+        return te;
+    }
+    void ap( vector < map<short,int>>*peo,map<short,int>*cand,vector<short>now)
+    {
+
+        float t=clock();
+
+        for(map<short,int>::iterator i=(*cand).begin(); i!=(*cand).end(); i++)
+        {
+            int n=0;
+
+            vector < map<short,int>>npeo;
+            map<short,int>ncand;
+
+
+
+
+            for(int j=0; j<(*peo).size(); j++)
+            {
+
+                 map<short,int>::iterator k=(*peo).at(j).find(i->first);
+                if(k!=(*peo).at(j).end())
+                {
+                    n++;
+                    // while((*peo).at(j).begin()->first!=i->first)
+                   {
+                       (*peo).at(j).erase((*peo).at(j).begin());
+                   }
+
+
+
+                    //cout<<"rr"<<peo.at(j).begin()->first<<endl;
+                   for(map<short,int>::iterator l=(*peo).at(j).begin();l!=(*peo).at(j).end();l++)
+                    {ncand.insert(pair<short,int>(l->first,1));}
+
+                    npeo.push_back((*peo).at(j));
                 }
             }
-        }
-        if(n>=50)
-        {
-            now.push_back(cand.at(i));
-            answer++;
-            //cout<<"si="<<now.size()<<endl;
-            /*for(int p=0;p<now.size();p++)
-            {cout<<now.at(p)<<" ";}
-            cout<<endl;*/
-
-
-
-                ap(npeo,cand,i+1,now);
-            now.pop_back();
-        }
-        /*else
-        {
-            if(now.size()>ma)
+            
+            if(n>=50)
             {
-                ma=now.size();
-                re.clear();
-                re.push_back(now);
+            for(int j=0; j<(*peo).size(); j++)
+                {
 
+                 map<short,int>::iterator k=(*peo).at(j).find(i->first);
+                if(k!=(*peo).at(j).end())
+                    {
+                   
+                    //cout<<"rr"<<peo.at(j).begin()->first<<endl;
+                   for(map<short,int>::iterator l=(*peo).at(j).begin();l!=(*peo).at(j).end();l++)
+                    {ncand.insert(pair<short,int>(l->first,1));}
+
+                    npeo.push_back((*peo).at(j));
+                        }
+                }
+                now.push_back(i->first);
+                answer++;
+
+                //for(int p=0;p<now.size();p++)
+                //{cout<<now.at(p)<<" ";}
+               // cout<<endl;
+
+
+
+
+                ap(&npeo,&ncand,now);
+                now.pop_back();
             }
-            if(now.size()==ma&&re.at(re.size()-1)!=now)
+            /*else
             {
-                re.push_back(now);
+                if(now.size()>ma)
+                {
+                    ma=now.size();
+                    re.clear();
+                    re.push_back(now);
 
-            }
-        }*/
-        n=0;
+                }
+                if(now.size()==ma&&re.at(re.size()-1)!=now)
+                {
+                    re.push_back(now);
+
+                }
+            }*/
+            n=0;
+
+
+        }
+        //aptime += (clock()-m  )/1000;
 
 
     }
-
-}
